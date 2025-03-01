fb36107fc88b3a0190e3d43d42a71e51588185bc
Pedro <pedhenriquecm@gmail.com>
2025-03-01 16:02:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Double-Short---ED2
---
+++ src/DoubleShort/DoubleShort.java
@@ -0,0 +1,34 @@
+package DoubleShort;
+import java.util.Arrays;
+import java.util.Random;
+
+public class DoubleShort {
+
+    public static void main(String[] args) {
+        int nt = 5;
+        int[] vetor = new int[nt];
+        Random rand = new Random();
+
+        for(int i = 0; i < nt; i++){
+            vetor[i] = rand.nextInt(101);
+        }
+
+        int flag = 1;
+        int tv = vetor.length;
+        while (flag != 0){
+            flag = 0;
+            for (int i = 0; i < tv - 1; i++) {
+                if(vetor[i] > vetor[i+1]){
+                    int bkp=vetor[i+1];
+                    vetor[i+1] = vetor[i];
+                    vetor[i] = bkp;
+                    flag = 1;
+                    System.out.println(Arrays.toString(vetor));
+                }
+            }
+        }
+        System.out.println("=============================================================");
+        System.out.println("Programa feito para organizar vetor pelo método Double Short!");
+        System.out.println("=============================================================");
+    }
+}
